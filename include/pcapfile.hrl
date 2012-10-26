a81019f2129765884cf00d62e6bd82922d8efa44
Artem Teslenko <ates@ipv6.dp.ua>
2012-10-26 14:56:08 +0300

    Add open/2, next/1, close/1

--- include/pcapfile.hrl
+++ include/pcapfile.hrl
@@ -1,6 +1,9 @@
 %%% http://wiki.wireshark.org/Development/LibpcapFileFormat
 
 -define(MAGIC, 16#a1b2c3d4).
+-define(SNAPLEN, 65535).
+-define(PCAP_HEADER_SIZE, 24).
+-define(PCAP_RECORD_HEADER_SIZE, 16).
 
 -define(PCAP_HEADER_LITTLE,
     ?MAGIC:4/little-unsigned-integer-unit:8,
@@ -20,19 +23,17 @@
     Snaplen:4/big-unsigned-integer-unit:8,
     Network:4/big-unsigned-integer-unit:8).
 
--define(PCAP_RECORD_LITTLE,
+-define(PCAP_RECORD_HEADER_LITTLE,
     Timestamp_s:4/little-unsigned-integer-unit:8,
     Timestamp_us:4/little-unsigned-integer-unit:8,
     InclLen:4/little-unsigned-integer-unit:8,
-    OrigLen:4/little-unsigned-integer-unit:8,
-    Payload:InclLen/binary-unit:8).
+    OrigLen:4/little-unsigned-integer-unit:8).
 
--define(PCAP_RECORD_BIG,
+-define(PCAP_RECORD_HEADER_BIG,
     Timestamp_s:4/big-unsigned-integer-unit:8,
     Timestamp_us:4/big-unsigned-integer-unit:8,
     InclLen:4/big-unsigned-integer-unit:8,
-    OrigLen:4/big-unsigned-integer-unit:8,
-    Payload:InclLen/binary-unit:8).
+    OrigLen:4/big-unsigned-integer-unit:8).
 
 -record(pcap_hdr, {
     order :: little | big,
@@ -45,7 +46,6 @@
 }).
 
 -record(pcap_record, {
-    seq :: non_neg_integer(),
     timestamp_s :: non_neg_integer(),
     timestamp_us :: non_neg_integer(),
     incl_len :: non_neg_integer(),
--- src/pcapfile.erl
+++ src/pcapfile.erl
@@ -1,77 +1,106 @@
 -module(pcapfile).
 
 %% API
--export([read_file/1]).
+-export([open/2, close/1, next/1, read_file/1]).
 
 -include("pcapfile.hrl").
 
 -spec read_file(string()) -> {ok, #pcap{}} | {error, term()}.
 read_file(Filename) ->
-    case file:read_file(Filename) of
-        {ok, Binary} ->
-            decode(Binary);
-        {error, Reason} ->
-            {error, Reason}
-    end.
-
-%% Internal functions
-
--spec decode(binary()) -> {ok, #pcap{}} | {error, term()}.
-decode(<<?PCAP_HEADER_LITTLE, Rest/binary>>) ->
-    Header = #pcap_hdr{
-        order = little,
-        major = Major,
-        minor = Minor,
-        gmt_to_localtime = GMT_to_localtime,
-        sigfigs = Sigfigs,
-        snaplen = Snaplen,
-        network = Network
-    },
-    Records = decode_records(little, Rest),
-    {ok, #pcap{header = Header, records = Records}};
-decode(<<?PCAP_HEADER_BIG, Rest/binary>>) ->
-    Header = #pcap_hdr{
-        order = big,
-        major = Major,
-        minor = Minor,
-        gmt_to_localtime = GMT_to_localtime,
-        sigfigs = Sigfigs,
-        snaplen = Snaplen,
-        network = Network
-    },
-    Records = decode_records(big, Rest),
+    {ok, Header, Device} = open(Filename, read),
+    {ok, Records} = read_records(Device),
     {ok, #pcap{header = Header, records = Records}}.
 
--spec decode_records(little | big, binary()) -> [#pcap_record{}].
-decode_records(Order, Binary) ->
-    decode_records(Order, Binary, 1, []).
+-spec open(string(), read | write) ->
+    {ok, #pcap_hdr{}, file:fd()} | {error, term()}.
+open(Filename, read) ->
+    case file:open(Filename, [read, binary]) of
+        {ok, Device} ->
+            {ok, Header} = read_header(Device),
+            {ok, Header, Device};
+        Error -> Error
+    end.
 
--spec decode_records(little | big, binary(), pos_integer(), list()) ->
-    [#pcap_record{}].
-decode_records(_Order, <<>>, _Seq, Acc) ->
-    lists:reverse(Acc);
-decode_records(little, <<?PCAP_RECORD_LITTLE, Rest/binary>>, Seq, Acc) ->
-    Record = #pcap_record{
-        seq = Seq,
-        timestamp_s = Timestamp_s,
-        timestamp_us = Timestamp_us,
-        incl_len = InclLen,
-        orig_len = OrigLen,
-        truncated = is_truncated(InclLen, OrigLen),
-        payload = Payload
-    },
-    decode_records(little, Rest, Seq + 1, [Record | Acc]);
-decode_records(big, <<?PCAP_RECORD_BIG, Rest/binary>>, Seq, Acc) ->
-    Record = #pcap_record{
-        seq = Seq,
-        timestamp_s = Timestamp_s,
-        timestamp_us = Timestamp_us,
-        incl_len = InclLen,
-        orig_len = OrigLen,
-        truncated = is_truncated(InclLen, OrigLen),
-        payload = Payload
-    },
-    decode_records(big, Rest, Seq + 1, [Record | Acc]).
+-spec close(file:fd()) -> ok.
+close(Device) ->
+    ok = file:close(Device).
+
+-spec next(file:fd()) -> {ok, #pcap_record{}}.
+next(Device) ->
+    read_record(Device).
+
+-spec read_header(file:fd()) -> {ok, #pcap_hdr{}} | {error, term()}.
+read_header(Device) ->
+    case file:read(Device, ?PCAP_HEADER_SIZE) of
+        {ok, <<?PCAP_HEADER_LITTLE>>} ->
+            Header = #pcap_hdr{
+                order = little,
+                major = Major,
+                minor = Minor,
+                gmt_to_localtime = GMT_to_localtime,
+                sigfigs = Sigfigs,
+                snaplen = Snaplen,
+                network = Network
+            },
+            {ok, Header};
+        {ok, <<?PCAP_HEADER_BIG>>} ->
+            Header = #pcap_hdr{
+                order = big,
+                major = Major,
+                minor = Minor,
+                gmt_to_localtime = GMT_to_localtime,
+                sigfigs = Sigfigs,
+                snaplen = Snaplen,
+                network = Network
+            },
+            {ok, Header}
+    end.
+
+-spec read_records(file:fd()) -> {ok, [#pcap_record{}]} | {error, term()}.
+read_records(Device) ->
+    read_records(Device, []).
+
+read_records(Device, Acc) ->
+    case read_record(Device) of
+        {ok, Record} ->
+            read_records(Device, [Record | Acc]);
+        eof ->
+            close(Device),
+            {ok, lists:reverse(Acc)};
+        Error ->
+            Error
+    end.
+
+-spec read_record(file:fd()) -> {ok, #pcap_record{}} | eof | {error, term()}.
+read_record(Device) ->
+    case file:read(Device, ?PCAP_RECORD_HEADER_SIZE) of
+        %% XXX: Should be fixed
+        {ok, <<?PCAP_RECORD_HEADER_LITTLE>>} when InclLen =< ?SNAPLEN ->
+            {ok, Payload} = file:read(Device, InclLen),
+            Record = #pcap_record{
+                timestamp_s = Timestamp_s,
+                timestamp_us = Timestamp_us,
+                incl_len = InclLen,
+                orig_len = OrigLen,
+                truncated = is_truncated(InclLen, OrigLen),
+                payload = Payload
+            },
+            {ok, Record};
+        {ok, <<?PCAP_RECORD_HEADER_BIG>>} ->
+            io:format("S: ~p~n", [InclLen]),
+            {ok, Payload} = file:read(Device, InclLen),
+            Record = #pcap_record{
+                timestamp_s = Timestamp_s,
+                timestamp_us = Timestamp_us,
+                incl_len = InclLen,
+                orig_len = OrigLen,
+                truncated = is_truncated(InclLen, OrigLen),
+                payload = Payload
+            },
+            {ok, Record};
+        Error ->
+            Error
+    end.
 
 -spec is_truncated(non_neg_integer(), non_neg_integer()) -> boolean().
 is_truncated(InclLen, OrigLen) -> InclLen < OrigLen.
